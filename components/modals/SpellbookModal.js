eca7cd9964139f7b7adf50c777e48de26153ead3
Eoin Lynagh <eoin.lynagh@techguilds.com>
2021-09-25 05:49:59 -0400

    removed spellbook confirm
    unnessescary to have 2 confirms

--- components/modals/SpellbookModal.js
+++ components/modals/SpellbookModal.js
@@ -30,14 +30,6 @@ export default function SpellbookModal({
         <div className="mb-2">{CardsList()}</div>
         <div className="flex mt-auto justify-evenly flex-col lg:flex-row-reverse">
           <button
-            className="text-gray-300 flex-1 py-2 px-4 rounded bg-red-600 m-3"
-            onClick={() => handleSpellbookModal()}
-          >
-            {currentAttackIndex === -1
-              ? "Exit"
-              : `Select ${character.attacks[currentAttackIndex].name}`}
-          </button>
-          <button
             className="text-gray-300 flex-1 py-2 px-4 rounded bg-green-600 m-3"
             onClick={() => handleBattleModal(handleSpellbookModal)}
           >
@@ -90,5 +82,6 @@ export default function SpellbookModal({
     } else {
       setCurrentAttackIndex(index);
     }
+    handleSpellbookModal();
   }
 }
