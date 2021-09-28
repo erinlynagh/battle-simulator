c4564b3affa3770d721eebe5cbf6d3af7537e912
Eoin Lynagh <eoin.lynagh@techguilds.com>
2021-09-28 17:53:39 -0400

    more responsive button

--- components/modals/Spellbook/SpellbookModal.js
+++ components/modals/Spellbook/SpellbookModal.js
@@ -41,7 +41,7 @@ export default function SpellbookModal({
 
         <div className="flex mt-auto justify-evenly  flex-col lg:flex-row-reverse">
           <button
-            className="text-gray-300 flex-1 py-2 px-4 rounded bg-green-600 m-3"
+            className="text-gray-300 flex-1 py-2 px-4 rounded bg-green-600 m-3 hover:text-green-600 hover:bg-gray-300"
             onClick={() => handleBattleModal(handleSpellbookModal)}
           >
             {showBattleModal ? "X" : emoji.getUnicode("eyes")}
